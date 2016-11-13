b97be00dc08e0b151a6703f6d2dc2359495cbc31
Felipe <fmejias19@gmail.com>
2016-11-12 20:17:49 -0600

    merge

--- ArduinoDriver/ArduinoCode/ArduinoCode.ino
+++ ArduinoDriver/ArduinoCode/ArduinoCode.ino
@@ -1,17 +1,21 @@
-
-String inputString = "";         // a string to hold incoming data
-boolean stringComplete = false;  // whether the string is complete
+/*Initial Data */
+String inputInstruction = "";         // a string to hold incoming data
+int posIni = 0; //contains the initial position
+int posFin = 0; //contains the final position
+int nextMove = 0; //contains the next move
+int sizeBoard = 0; //contains the size of the board
+boolean instructionComplete = false;  // whether the string is complete
 
 void setup() {
   // initialize serial:
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
   // reserve 200 bytes for the inputString:
-  inputString.reserve(200);
+  inputInstruction.reserve(200);
 }
 
 void loop() {
-    stringComplete = false;
+    instructionComplete = false;
 }
 
 /*
@@ -28,18 +32,32 @@ void serialEventRun(void) {
 void serialEvent() {
   while (Serial.available()) {
     char inChar = (char)Serial.read();
-    inputString += inChar;
+    inputInstruction += inChar;
+    instructionComplete = false;
+    
+    if(inputInstruction.length() == 5 && inputInstruction != "Board"){ 
+      digitalWrite(LED_BUILTIN, HIGH);
+      posIni = inputInstruction[0] - '0';
+      posFin = inputInstruction[2] - '0';
+      nextMove = inputInstruction[4] - '0';
+      Serial.write("L");
+      inputInstruction = "";
+    }
+  if(inputInstruction.length() == 7){ 
+      digitalWrite(LED_BUILTIN, HIGH);
+      sizeBoard = inputInstruction[6] - '0';
+      Serial.write("L");
+      inputInstruction = "";
+    }
+  }
  
-    Serial.println(inputString);
-    if(inputString.equals("Hola")){ 
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println(inputString);
-    delay(1000);
-    inputString = "";
-   }
-   digitalWrite(LED_BUILTIN, LOW);
+}
+
+void writeString(String stringData) { // Used to serially push out a String with Serial.write()
+
+  for (int i = 0; i < stringData.length(); i++)
+  {
+    Serial.write(stringData[i]);   // Push each char 1 by 1 on each loop pass
   }
 
-  
-  
-}
+}// end writeString
