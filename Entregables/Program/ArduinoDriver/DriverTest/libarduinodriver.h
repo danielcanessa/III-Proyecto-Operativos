29fb292de9ef9bd7ce0aa883cabd7bb4e153689e
dcanessa <danielcanessa09@gmail.com>
2016-11-15 15:06:32 -0600

    final

--- Entregables/Program/ArduinoDriver/DriverTest/libarduinodriver.h
+++
@@ -1,8 +0,0 @@
-/*
-   libarduinodriver.h:
-   routines in libarduinodriver.a
-*/
-
-extern int arduino_send_message (char stringToSend[256]);
-extern int arduino_start_com();
-
--- Entregables/Program/ArduinoDriver/DriverTest/test_driver.c
+++
@@ -1,9 +0,0 @@
-#include "libarduinodriver.h"
-
-/* To shorten example, not using argp */
-int main ()
-{
-  arduino_start_com(); //Open the file of the Arduino
-  arduino_send_message ("Hola"); //Send this message to the arduino
-  return 0;
-}
